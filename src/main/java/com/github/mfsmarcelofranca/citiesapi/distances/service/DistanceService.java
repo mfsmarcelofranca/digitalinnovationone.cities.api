3e663160ec3fe164c402bbcd81a3635354ae5480
Marcelo Franca <mfsmarcelo@yahoo.com.br>
2021-05-14 23:30:10 -0300

    parte12 até parte13

---
+++ src/main/java/com/github/mfsmarcelofranca/citiesapi/distances/service/DistanceService.java
@@ -0,0 +1,53 @@
+package com.github.mfsmarcelofranca.citiesapi.distances.service;
+
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
+
+import com.github.mfsmarcelofranca.citiesapi.cities.entities.entities.City;
+import com.github.mfsmarcelofranca.citiesapi.cities.entities.repositories.CityRepository;
+import com.github.mfsmarcelofranca.citiesapi.utils.StringLocationUtils;
+import org.slf4j.LoggerFactory;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.geo.Point;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DistanceService {
+
+    private final CityRepository cityRepository;
+    Logger log = LoggerFactory.getLogger(DistanceService.class);
+
+    public DistanceService(final CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    public Double distanceByPointsInMiles(final Long city1, final Long city2) {
+        log.info("nativePostgresInMiles({}, {})", city1, city2);
+        return cityRepository.distanceByPoints(city1, city2);
+    }
+
+    public Double distanceByCubeInMeters(final Long city1, final Long city2) {
+        log.info("distanceByCubeInMeters({}, {}, {})", city1, city2);
+        final List<City> cities = cityRepository.findAllById((Arrays.asList(city1, city2)));
+
+        Point p1 = cities.get(0).getLocation();
+        Point p2  = cities.get(1).getLocation();
+
+        return cityRepository.distanceByCube(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    }
+
+   /* public Double distanceUsingMath(Long city1, Long city2, EarthRadius unit) {
+        log.info("distanceByCubeInMeters({}, {}, {})", city1, city2);
+        final List<City> cities = cityRepository.findAllById((Arrays.asList(city1, city2)));
+        Point p1 = cities.get(0).getLocation();
+        Point p2  = cities.get(1).getLocation();
+        return cityRepository.distanceByCube(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    }*/
+}
